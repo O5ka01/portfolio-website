1f04eeb4a72f6e239104f80ddebbfecd7cab868f
O5ka01 <ole.heinrichs.privat@gmail.com>
2025-05-01 14:47:27 +0200

    locale error fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['en', 'de'],
+    defaultLocale: 'de',
+    localeDetection: true
+  },
   images: {
     formats: ['image/avif', 'image/webp'], // Enable modern image formats
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048], // Responsive image sizes
